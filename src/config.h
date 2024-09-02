c5cfbc043ec47a70d301f08cbefa93417f697201
ccccr <hy.ccccr@gmail.com>
2024-09-02 18:50:17 +0800

    feat: :tada: init project

---
+++ src/config.h
@@ -0,0 +1,7 @@
+// 0: At8236 brushless HID mode
+// TODO: 1: At8236 bruseless Serial mode
+// TODO: 2: At8236 HID mode
+// TODO: 3: At8236 Serial mode
+// TODO: 4: L298N HID mode
+// TODO: 5: L298N Serial mode
+#define MODE 0
