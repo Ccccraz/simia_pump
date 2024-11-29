2f80f47bf5ed94816b197f2357f9dff933b78908
Ccccraz <hy.ccccr@gmail.com>
2024-11-29 23:37:13 +0800

    feat: :sparkles: spuorrt for brushed pump hid mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,21 @@
 #include <USB.h>
 
 // at8236 control pins
-constexpr uint8_t first_pin{1};
-constexpr uint8_t second_pin{2};
+constexpr uint8_t first_pin{8};
+constexpr uint8_t second_pin{18};
 
 // system buttons
-constexpr uint8_t start_button{7};
-constexpr uint8_t stop_button{8};
-constexpr uint8_t reverse_button{9};
-constexpr uint8_t speed_button{10};
+constexpr uint8_t start_button{40};
+constexpr uint8_t stop_button{41};
+constexpr uint8_t reverse_button{42};
 
 #if MODE == 0
 
-#include "at8236_brushless_hid.h"
+// #include "at8236_brushless_hid.h"
+#include "at8236_hid.h"
 
-// komoer brushless motor control pins
-constexpr uint8_t speed_ctrl_pin{3};
-constexpr uint8_t direction_ctrl_pin{4};
-constexpr uint8_t report_pin{5};
-float speed{0.0f};
-
-simia::AT8236BrushlessHID pump(first_pin, second_pin, 0, speed_ctrl_pin, report_pin, direction_ctrl_pin);
+// simia::AT8236BrushlessHID pump(first_pin, second_pin, 0, speed_ctrl_pin, report_pin, direction_ctrl_pin);
+simia::AT8236HID pump(first_pin, second_pin, 1.0f);
 
 #endif
 
@@ -47,7 +42,6 @@ void setup()
     pinMode(start_button, INPUT_PULLUP);
     pinMode(stop_button, INPUT_PULLUP);
     pinMode(reverse_button, INPUT_PULLUP);
-    pinMode(speed_button, INPUT_PULLDOWN);
 
     attachInterrupt(digitalPinToInterrupt(start_button), start, FALLING);
     attachInterrupt(digitalPinToInterrupt(stop_button), stop, FALLING);
@@ -62,13 +56,4 @@ void setup()
 
 void loop()
 {
-    // Speed control
-    auto val = analogRead(speed_button);
-    if (val - 50 > speed || val + 50 < speed)
-    {
-        speed = val;
-        pump.set_speed(val / 4095.0f);
-    }
-
-    delay(10);
 }
